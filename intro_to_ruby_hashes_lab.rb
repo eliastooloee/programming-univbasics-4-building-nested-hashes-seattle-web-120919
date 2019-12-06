0244fad8d6f3f74b89ab0b76b066e0ed38226554
Elias Tooloee <elitooloee@gmail.com>
2019-12-06 12:52:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,9 @@ def monopoly_with_third_tier
   	   :pieces => 4,
   	   :names => {
   	     :reading_railroad => {:mortgage_value => "$100"},
-  	     :shortline_railroad => {},
-  	     :pennsylvania_railroad => {},
-  	     :b_and_o_railroad => {}
+  	     :shortline_railroad => {:mortgage_value => "$100"},
+  	     :pennsylvania_railroad => {:mortgage_value => "$100"},
+  	     :b_and_o_railroad => {:mortgage_value => "$100"}
   	   },
   	   :rent_in_dollars => {
   	     :one_piece_owned => 25,
